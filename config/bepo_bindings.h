e78d6f782b727e1d6688c70089ee95fda3b7349b
clpm <cai.lpmiranda@gmail.com>
2022-05-21 11:31:45 +0200

    Create bepo_bindings.h

---
+++ config/bepo_bindings.h
@@ -0,0 +1,100 @@
+/* BINDINGS FOR BÉPO */
+#define B_		Q
+#define EACT_	W
+#define P_		E
+#define O_		R
+#define EGRV_	T
+#define CFLX_	Y
+#define V_		U
+#define D_		I
+#define L_		O
+#define J_		P
+#define Z_		LBKT
+#define W_		RBKT
+
+#define A_		A
+#define U_		S
+#define I_		D
+#define E_		F
+#define COMM_	G
+#define C_		H
+#define T_		J
+#define S_		K
+#define R_		L
+#define N_		SEMI
+#define M_		APOSTROPHE
+#define CCED_	BACKSLASH
+
+#define AGRV_	Z
+#define Y_		X
+#define X_		C
+#define DOT_	V
+#define K_		B
+#define APSC_	N /* inverted comma */
+#define APST_	RA(COMM_)
+#define Q_		M
+#define G_		COMMA
+#define H_		DOT
+#define F_		SLASH
+
+#define EXCL_	LS(CFLX_)
+#define SEMI_	LS(COMM_)
+#define COLN_	LS(DOT_)
+#define QMRK_	LS(APSC_)
+
+#define ACUT_	RA(EACT_)
+#define GRAV_	RA(EGRV_)
+#define TILD_	RA(N_)
+#define AMPR_	RA(P_)
+#define OE_		RA(O_)
+
+#define AE_		RA(A_)
+#define TRMA_	RA(I_)
+#define EURO_	RA(E_)
+#define BSLA_	RA(AGRV_)
+#define LCRL_	RA(Y_)
+#define RCRL_	RA(X_)
+#define ULIN_	RA(SPACE)
+
+#define DLLR_	GRAVE
+#define HASH_	LS(DLLR_)
+#define QUOT_	N1
+#define LGLM_	N2
+#define RGLM_	N3
+#define LT_		RA(LGLM_)
+#define GT_		RA(RGLM_)
+#define LPAR_	N4
+#define RPAR_	N5
+#define LBRK_	RA(LPAR_)
+#define RBRK_	RA(RPAR_)
+#define AT_		N6
+#define PLUS_	N7
+#define MIN_	N8
+#define FSLA_	N9
+#define STAR_	N0
+#define EQUL_	MINUS
+#define PCNT_	EQUAL
+
+#define PIPE_	RA(B_)
+
+#define CRAT_ RA(N6_)
+#define TLDL_ RA(K_) /* loose tilde */
+
+
+#define MDSH_  RA(N8)
+#define NDSH_  RA(N1)
+
+#define NBSP_  LS(SPACE)
+
+#define N0_  LS(N0)
+#define N1_  LS(N1)
+#define N2_  LS(N2)
+#define N3_  LS(N3)
+#define N4_  LS(N4)
+#define N5_  LS(N5)
+#define N6_  LS(N6)
+#define N7_  LS(N7)
+#define N8_  LS(N8)
+#define N9_  LS(N9)
+		
+#define PSBRK PAUSE_BREAK
